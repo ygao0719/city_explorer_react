b0126348ab60e3899fe2ce97141c43b57bdb89b6
Yuan Gao <example@email.com>
2019-05-08 10:30:49 -0700

    update city explorer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+class App extends React.Component {
+  render(){
+    return (
+    <React.Fragment>
+      <Header />
+      <Main />
+    </React.Fragment>
+    );
+  }
+}
+
+class Header extends React.Component{
+  render(){
+    return (
+      <React.Fragment>
+        <h1> city explorer</h1>
+        <p>Enter a location below to learn about the weather, events, restaurants, movies filmed there, and more!</p>
+      </React.Fragment>
+    );
+  }
+  
+}
+
+class Main extends React.Component{
+  render(){
+    return (
+      <React.Fragment>
+        <Search />
+        <Map/>
+        <Result/>
+        <Result/>
+        <Result/>
+        <Result/>
+        <Result/>
+      </React.Fragment>
+    );
+  }
+
+}
+
+class Search extends React.Component{
+render(){
+  return(
+    <React.Fragment>
+      <input type = "text" name="search"></input>
+      <button> search </button>
+    </React.Fragment>
   );
+  
+}
+  
+}
+
+class Map extends React.Component{
+  render(){
+    return(
+      <React.Fragment>
+        <div>map</div>
+      </React.Fragment>
+    );
+  }
+}
+
+class Result extends React.Component{
+  render(){
+    return(
+      <React.Fragment>
+        <section>Result</section>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
