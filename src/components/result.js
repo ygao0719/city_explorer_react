de9a5db34ec89e328692e892604032bf78f4f9c5
Yuan Gao <example@email.com>
2019-05-10 13:02:49 -0700

    get map darksky and yelp

---
+++ src/components/result.js
@@ -0,0 +1,26 @@
+// import React from 'react';
+
+// class Result extends React.component{
+//   render(){
+//     return(
+//       <div>
+//       {/* <DarkSky/> */}
+//       </div>
+//     );
+//   }
+
+// }
+
+
+// // class DarkSky extends React.component{
+
+// //   render(){
+// //     return (
+// //       <div></div>
+// //     );
+// //   }
+
+
+
+// // }
+// // export default Result;
